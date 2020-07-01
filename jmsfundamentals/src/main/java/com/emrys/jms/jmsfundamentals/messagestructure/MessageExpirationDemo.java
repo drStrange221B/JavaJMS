86394bede9a3be511fbe13ff8ffa5e580e75be66
Rojaya Maharjan <rojaya.mh3@gmail.com>
2020-06-30 23:58:59 -0400

    set timeout by using property in producer setTimeToLive()

---
+++ jmsfundamentals/src/main/java/com/emrys/jms/jmsfundamentals/messagestructure/MessageExpirationDemo.java
@@ -0,0 +1,42 @@
+package com.emrys.jms.jmsfundamentals.messagestructure;
+
+import javax.jms.JMSConsumer;
+import javax.jms.JMSContext;
+import javax.jms.JMSProducer;
+import javax.jms.Message;
+import javax.jms.Queue;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
+
+public class MessageExpirationDemo {
+
+	public static void main(String[] args) throws NamingException, InterruptedException {
+		
+		InitialContext context = new InitialContext();
+		Queue queue = (Queue)context.lookup("queue/myQueue");
+		
+		try(ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
+			JMSContext jmsContext =	cf.createContext()){
+			
+			JMSProducer producer = jmsContext.createProducer();
+			producer.setTimeToLive(2000);
+			TextMessage createTextMessage = jmsContext.createTextMessage("Sample message from Producer");
+			producer.send(queue, createTextMessage);
+			
+			Thread.sleep(5000);
+			
+			JMSConsumer createConsumer = jmsContext.createConsumer(queue);
+			Message receiveBody = createConsumer.receive(2000);
+			
+			System.out.println(receiveBody + "   checking...");
+			
+		}
+		
+		
+
+	}
+
+}
